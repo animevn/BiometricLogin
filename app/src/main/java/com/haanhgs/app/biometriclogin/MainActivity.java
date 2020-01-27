e6c36ae85899b3bb3dc109c3e962483d1f44dd1f
animevn <haanhth21@gmail.com>
2020-01-27 09:29:59 +0700

    ok now

--- app/src/main/java/com/haanhgs/app/biometriclogin/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/biometriclogin/MainActivity.java
@@ -1,13 +1,19 @@
 package com.haanhgs.app.biometriclogin;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-
+import java.util.concurrent.Executor;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.pbrMain)
     ProgressBar pbrMain;
 
+    private static final String DTAG = "D.MainActivity";
+    private static final String ETAG = "E.MainActivity";
+    private BiometricPrompt.PromptInfo prompt;
+    private BiometricPrompt biometricPrompt;
+
     public void showViews() {
         bnSignIn.setVisibility(View.VISIBLE);
         pbrMain.setVisibility(View.INVISIBLE);
@@ -29,11 +40,40 @@ public class MainActivity extends AppCompatActivity {
         pbrMain.setVisibility(View.INVISIBLE);
     }
 
+    private void getPromptInfo(){
+        prompt = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setConfirmationRequired(true)
+                .setDeviceCredentialAllowed(true)
+                .build();
+    }
+
+    private void checkBiometricsHardware(){
+        BiometricManager manager = BiometricManager.from(this);
+        switch (manager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d(DTAG, "App can authenticate using biometrics.");
+                getPromptInfo();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e(ETAG, "Biometric features are currently unavailable.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e(ETAG, "Biometric features are currently unavailable.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Log.e(ETAG, "The user hasn't associated any biometric credentials.");
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        showViews();
+        checkBiometricsHardware();
     }
 
     private void listenToBackPressedWhenInFragment() {
@@ -50,7 +90,50 @@ public class MainActivity extends AppCompatActivity {
         listenToBackPressedWhenInFragment();
     }
 
+    private void openFragment(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag("home");
+        if (fragment == null){
+            FragmentHome fragmentHome = new FragmentHome();
+            ft.replace(R.id.clHome, fragmentHome, "home").commit();
+        }else {
+            ft.attach(fragment);
+        }
+    }
+
+    private void getBioMetricsPrompt(){
+        Executor executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(this, executor,
+                new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Log.e(ETAG, errString.toString());
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                //for encrypt user data or sth, here is unneccessary
+                BiometricPrompt.CryptoObject authenticatedCryptoObject = result.getCryptoObject();
+                hideViews();
+                openFragment();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Log.e(ETAG, "authentication failed");
+            }
+        });
+    }
+
     @OnClick(R.id.bnSignIn)
     public void onViewClicked() {
+        if (prompt != null) {
+            getBioMetricsPrompt();
+            biometricPrompt.authenticate(prompt);
+        }
     }
 }
